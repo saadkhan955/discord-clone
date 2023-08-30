7e54ac80110aaeaa38aee711eaea5bfffdec6f7c
Saad Khan <saad.khan955@gmail.com>
2023-08-30 23:42:40 +0530

    Added the manage member functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -70,6 +70,7 @@ export const ServerHeader = ({
         )}
         {isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen("members", { server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Manage Members
